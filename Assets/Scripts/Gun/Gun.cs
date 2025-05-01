e7947a3974d38ba2d575748569743520051fe59f
Bulut Yankı Karahan <bulutmac8@hotmail.com>
2025-05-02 00:53:28 +0300

    Added guns and changed camera view

---
+++ Assets/Scripts/Gun/Gun.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class Gun : MonoBehaviour
+{
+    public float damage = 10f;
+    public float range = 100f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        if(Input.)
+    }
+    
+    
+    
+    
+    
+}
