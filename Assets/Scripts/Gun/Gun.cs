18d1fb88884e9906a3491024c12029e67d06c6b3
Bulut Yankı Karahan <bulutmac8@hotmail.com>
2025-05-02 04:33:06 +0300

    Added on hit effects

--- Assets/Scripts/Gun/Gun.cs
+++ Assets/Scripts/Gun/Gun.cs
@@ -4,13 +4,17 @@ public class Gun : MonoBehaviour
 {
     public float damage = 10f;
     public float range = 100f;
-
+    public float fireRate = 15f;
     public Camera fpsCam;
-    // Update is called once per frame
+    public ParticleSystem muzzleFlash;
+    public GameObject impactEffect;
+
+    private float nextTimeToFire = 0f;
     void Update()
     {
-        if (Input.GetButtonDown("Fire1"))
+        if (Input.GetButton("Fire1")&&Time.time>= nextTimeToFire)
         {
+            nextTimeToFire = Time.time + 1f / fireRate;
             Shoot();
         }
         
@@ -18,6 +22,7 @@ public class Gun : MonoBehaviour
     
      void Shoot()
      {
+         muzzleFlash.Play();
          RaycastHit hit;
         
          if (Physics.Raycast( fpsCam.transform.position, fpsCam.transform.forward, out hit, range))
@@ -31,6 +36,9 @@ public class Gun : MonoBehaviour
                  enemy.TakeDamage(damage);
              }
 
+             
+             GameObject destroyEffect =Instantiate(impactEffect, hit.point, Quaternion.LookRotation(hit.normal));
+             Destroy(destroyEffect,0.3f);
          }
 
 
