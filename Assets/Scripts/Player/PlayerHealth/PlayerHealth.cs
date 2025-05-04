28d5cd180379ea80112ed3bd524bae25e95e904c
Bulut Yankı Karahan <bulutmac8@hotmail.com>
2025-05-04 15:10:01 +0300

    Added HealthBar UI and RestoreHealth method.

--- Assets/Scripts/Player/PlayerHealth/PlayerHealth.cs
+++ Assets/Scripts/Player/PlayerHealth/PlayerHealth.cs
@@ -2,28 +2,84 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using Random = UnityEngine.Random;
 
 public class PlayerHealth : MonoBehaviour
-{
+{   
+    private float health;
+    private float lerpTimer;
     [SerializeField] public int maxHealth;
-    [SerializeField] private float health;
-
+    public float chipSpeed =2f;
+    public Image frontHealthBar;
+    public Image backHealthBar;
+   
+        
     private void Start()
     {
         health = maxHealth;
     }
 
+     void Update()
+    {
+        health = Mathf.Clamp(health, 0, maxHealth);
+        UpdateHealthUI();
 
-    public void TakeDamage(int amount)
+        if (Input.GetKeyDown(KeyCode.H))
+        {
+            TakeDamage(25);
+        }
+        if (Input.GetKeyDown(KeyCode.K))
+        {
+            RestoreHealth(25);
+        }
+    }
+
+    public void UpdateHealthUI()
+    {
+        float fillFront = frontHealthBar.fillAmount;
+        float fillBack = backHealthBar.fillAmount;
+        float hFraction = health / maxHealth;
+        if (fillBack>hFraction)
+        {
+            frontHealthBar.fillAmount = hFraction;
+            backHealthBar.color = Color.red;
+            lerpTimer += Time.deltaTime;
+            float percentComplete = lerpTimer / chipSpeed;
+            percentComplete *= percentComplete;
+            backHealthBar.fillAmount = Mathf.Lerp(fillBack, hFraction, percentComplete);
+        }
+
+        if (fillFront < hFraction)
+        {
+            backHealthBar.fillAmount = hFraction;
+            backHealthBar.color = Color.green;
+            lerpTimer += Time.deltaTime;
+            float percentComplete = lerpTimer / chipSpeed;
+            percentComplete *= percentComplete;
+            frontHealthBar.fillAmount = Mathf.Lerp(fillFront, backHealthBar.fillAmount, percentComplete);
+        }
+    }
+
+    public void TakeDamage(float amount)
     {
         health -= amount;
+        lerpTimer = 0f;
         if (health == 0)
         {
             Die();
         }
     }
 
-    
+    public void RestoreHealth(float healAmount)
+    {
+        health += healAmount;
+        lerpTimer = 0f;
+        if (health > 100)
+        {
+            health = 100;
+        }   
+    }
     public void Die()
     {
         
